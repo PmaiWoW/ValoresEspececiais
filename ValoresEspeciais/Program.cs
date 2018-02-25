b287fcf739b8aee65a140cdf5b84dabca04ef6ca
PmaiWoW <PmaiWoW@Gmail.com>
2018-02-25 19:56:45 +0000

    Valores Especiais - Máxs, Mins e Infinitos

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -10,6 +10,28 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
+            byte b = byte.MaxValue;
+            double d = double.MaxValue;
+            float f = float.MaxValue;
+            int i = int.MaxValue;
+            uint ui = uint.MaxValue;
+            short s = short.MaxValue;
+            sbyte sb = sbyte.MaxValue;
+            long l = long.MaxValue;
+            ulong ul = ulong.MaxValue;
+            Console.WriteLine($"Maximum Values:\nbyte:{b}\ndouble:{d}\nint:{i}uint:{ui}\nshort:{s}\nsbyte:{sb}\nlong:{l}\nulong:{ul}n\n");
+            b = byte.MinValue;
+            d = double.MinValue;
+            f = float.MinValue;
+            i = int.MinValue;
+            ui = uint.MinValue;
+            s = short.MinValue;
+            sb = sbyte.MinValue;
+            l = long.MinValue;
+            ul = ulong.MinValue;
+            Console.WriteLine($"Minimum Values:\nbyte:{b}\ndouble:{d}\nint:{i}uint:{ui}\nshort:{s}\nsbyte:{sb}\nlong:{l}\nulong:{ul}\n\n");
+            Console.WriteLine($"Negative Infinity (Double): {d / 0}\nPositive Infinity (Double): {d / 0}\nNegative Infinity (Float): {f / 0}\nPositive Infinity (Float): {f / 0}\nInfinity/Infinity: {double.PositiveInfinity / double.PositiveInfinity}\n\n");
+            Console.ReadKey();
         }
     }
 }
